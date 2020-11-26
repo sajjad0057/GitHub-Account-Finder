307231f609de3cb3e02d50d872d6fb34d64a3692
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-11-27 02:06:58 +0600

    using git API

--- API_examples/views.py
+++ API_examples/views.py
@@ -14,15 +14,3 @@ def index(request):
     return render(request,'index.html',context={'user':user})
 
 
-def geo(request):
-    pass
-    # is_cached = ('geodata' in request.session)
-    #
-    # if not is_cached:
-    #     ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
-    #     print('sajjjjjjjjjjjjad',ip_address)
-    #     response = requests.get('http://freegeoip.net/json/%s' % ip_address)
-    #     request.session['geodata'] = response.json()
-    #
-    # geodata = request.session['geodata']
-    return render(request,'geo.html',)
